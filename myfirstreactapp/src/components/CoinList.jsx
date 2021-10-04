196312abfc2709f4d73a4cdda50bb0f75111458f
Nick <nick.c.ruzicka@gmail.com>
2021-10-04 17:25:47 -0400

    updated api

--- myfirstreactapp/src/components/CoinList.jsx
+++ myfirstreactapp/src/components/CoinList.jsx
@@ -9,7 +9,7 @@ const CoinList = () => {
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false&price_change_percentage=24h%2C%207d%2C%2014d"
       )
       .then((res) => {
         setCoins(res.data);
